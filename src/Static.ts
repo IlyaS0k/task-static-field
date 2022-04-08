5ab6985a43bb1e34e5139fb363ef439cfe219ddd
IlyaS0k <ilyasokolov63@mail.ru>
2022-04-08 21:31:51 +0300

    task-static-field

--- src/Static.ts
+++ src/Static.ts
@@ -4,13 +4,4 @@
 3. Добавьте публичный статический метод `PI`, возвращающий значение `Math.PI`
 4. Добавьте публичный статический метод `ceil`, возвращающий число округленное в большую сторону
  */
-export default class TestClass {
-    public counter: number = 0;
-    public static counterStatic = 0;
-    public static PI() {
-        return Math.PI;
-    }
-    public static ceil(number: number) {
-        return Math.ceil(number);
-    }
-}
+export default class TestClass {}
